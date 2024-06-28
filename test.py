65f7f434646aaf774d1c69e3090ce3b591e64989
yinfuqian <2312077914@qq.com>
2024-06-28 11:21:15 +0800

    modify

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # 英文到中文键的映射
 key_translation = {
     "in": "公称通径",
-    "l": "长度",
+    "l": "固定式球阀长度",
     "w": "孔中心",
     "o": "法兰外径",
     "r": "水线台阶",
@@ -23,7 +23,12 @@ key_translation = {
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # 读取本地JSON文件并获取所有sheet_name
+    with open('output.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    sheet_names = [sheet['sheet_name'] for sheet in data]
+
+    return render_template('index.html', sheet_names=sheet_names)
 
 
 @app.route('/post', methods=['POST'])
@@ -41,14 +46,14 @@ def post():
             for item in sheet['data']:
                 if item['in'] == in_value:
                     # 翻译键
-                    translated_result = {key_translation.get(key, key): value for key, value in item.items() if
-                                         key != 'in'}
-                    response =  jsonify(translated_result)
+                    translated_result = {key_translation.get(key, key): value for key, value in item.items() if key != 'in'}
+                    response = jsonify(translated_result)
                     response.headers["Content-Type"] = "application/json; charset=utf-8"
                     return response
+
             return jsonify({"error": "没有找到对应的通经"})
     return jsonify({"error": "没有找到对应的名称"})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
